d64ddcb88c734ac942e095e3bc2a4fb683036e67
Salman <md.salman@cefalo.com>
2026-05-13 19:31:17 +0600

    chore(deps): upgrade angular to v18

--- src/app/app-root/app-root.module.ts
+++ src/app/app-root/app-root.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { AppRootRoutingModule } from './app-root-routing.module';
 import { RootDefaultComponent } from './components/root-default/root-default.component';
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
-@NgModule({
-  declarations: [
-    RootDefaultComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRootRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-  ],
-  bootstrap: [
-    RootDefaultComponent
-  ],
-})
+@NgModule({ declarations: [
+        RootDefaultComponent
+    ],
+    bootstrap: [
+        RootDefaultComponent
+    ], imports: [BrowserModule,
+        AppRootRoutingModule,
+        BrowserAnimationsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppRootModule {
 }
